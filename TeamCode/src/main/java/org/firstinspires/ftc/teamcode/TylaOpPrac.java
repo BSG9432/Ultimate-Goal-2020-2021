42910963c8509c902029764a312cdabd98beabb2
Bri <43770921+sconeheir@users.noreply.github.com>
2020-09-29 17:52:28 -0700

    TeleOp Practice 9/29/2020

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TylaOpPrac.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+@TeleOp (name = "TylaOpPrac")
+public class TylaOpPrac extends OpMode {
+    DcMotor backLeft;
+    DcMotor backRight;
+    DcMotor frontRight;
+    DcMotor frontLeft;
+
+    @Override
+    public void init() {
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+        backRight = hardwareMap.dcMotor.get("backLeft");
+        frontRight = hardwareMap.dcMotor.get("backLeft");
+        frontLeft = hardwareMap.dcMotor.get("backLeft");
+
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+       backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+    }
+
+    @Override
+    public void loop() {
+        if (Math.abs(gamepad1.left_stick_y) > .1) {
+            backLeft.setPower(-gamepad1.left_stick_y);
+            frontLeft.setPower(-gamepad1.left_stick_y);
+
+
+        }
+        else {
+
+            backLeft.setPower(0);
+            frontLeft.setPower(0);
+    }
+        if (Math.abs(gamepad1.right_stick_y) > .1) {
+            backRight.setPower(-gamepad1.right_stick_y);
+            frontRight.setPower(-gamepad1.right_stick_y);
+
+        }
+        else {
+            backRight.setPower(0);
+            frontRight.setPower(0);
+        }
+
+              {
+        }} }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ailaPrac.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@TeleOp
+public class ailaPrac extends OpMode {
+    DcMotor frontLeft;
+    DcMotor backLeft;
+    DcMotor frontRight;
+    DcMotor backRight;
+
+    @Override
+    public void init() {
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        backRight = hardwareMap.dcMotor.get("backRight");
+
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    @Override
+    public void loop() {
+if (Math.abs)
+    }
+}
