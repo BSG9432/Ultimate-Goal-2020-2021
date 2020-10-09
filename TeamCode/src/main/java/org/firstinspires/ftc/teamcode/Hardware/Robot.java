e6ffbda91babf667b65a8dcc184abd706650970f
riceangel <43398049+riceangel@users.noreply.github.com>
2020-10-08 19:07:34 -0700

    Added folders for organization | Created Robot.java hardware class (WIP)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Robot.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.Hardware;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class Robot {
+    //Hardware
+    public DcMotor frontLeft;
+    public DcMotor frontRight;
+    public DcMotor backLeft;
+    public DcMotor backRight;
+
+    public void initDriveTrain(HardwareMap hardwareMap) {
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+        backRight = hardwareMap.dcMotor.get("backRight");
+    }
+
+    public void encoderDrive(double speed, double leftInches,
+                             double rightInches, double timeout){
+        //Stay tuned :D
+    }
+}
