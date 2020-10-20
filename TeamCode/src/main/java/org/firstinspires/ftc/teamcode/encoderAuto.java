5859149bf45e82a01721497755388eab00a2cc08
Bri <43770921+sconeheir@users.noreply.github.com>
2020-10-20 16:23:12 -0700

    added encoderExample and worked on encoderExample
    encoderExample - practice
    encoderAuto - non vision functions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/encoderAuto.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous
+public class encoderAuto extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
