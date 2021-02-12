228c540554bcc1c792a0c2d8f704af8f5ecfb8a0
riceangel <43398049+riceangel@users.noreply.github.com>
2021-02-11 16:38:20 -0800

    Fix EasyOpenCVTest (name changed to EasyOpenCVRedTest)
    - When Jet tried to fix the stash errors, he kept all of the new changes with the stashed changes, which ended up leaving a lot of errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/EasyOpenCVRedTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/EasyOpenCVRedTest.java
@@ -26,6 +26,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.Hardware.Robot;
+import org.firstinspires.ftc.teamcode.Vision.PipelineTest;
+
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
@@ -36,15 +38,14 @@ import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 @TeleOp
-public class EasyOpenCVTest extends LinearOpMode {
+public class EasyOpenCVRedTest extends LinearOpMode {
     Robot bsgBot = new Robot();
     OpenCvCamera webcam; //named webby in config
 
     PipelineTest pipeline;
 
     @Override
-    public void runOpMode()
-    {
+    public void runOpMode() {
         //initialize robot hardware
         bsgBot.initRobot(hardwareMap);
 
@@ -64,11 +65,9 @@ public class EasyOpenCVTest extends LinearOpMode {
          * better behavior when pressing stop during init (i.e. less of a chance
          * of tripping the stuck watchdog)
          */
-        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
-        {
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
-            public void onOpened()
-            {
+            public void onOpened() {
                 /*
                  * Tell the webcam to start streaming images to us! Note that you must make sure
                  * the resolution you specify is supported by the camera. If it is not, an exception
@@ -94,7 +93,7 @@ public class EasyOpenCVTest extends LinearOpMode {
                  * (while a streaming session is in flight) *IS* supported.
                  */
 
-                webcam.setPipeline(new PipelineTest());
+                webcam.setPipeline(pipeline);
             }
         });
         // Tell telemetry to update faster than the default 250ms period :)
@@ -109,8 +108,7 @@ public class EasyOpenCVTest extends LinearOpMode {
 
 
         //Manages Telemetry and stopping the stream
-        while (opModeIsActive())
-        {
+        while (opModeIsActive()) {
             /*
              * Send some stats to the telemetry
              */
@@ -125,101 +123,21 @@ public class EasyOpenCVTest extends LinearOpMode {
             telemetry.update();
 
 
-            //The "if" statement below will stop streaming from the camera when the "A" button on gamepad 1 is pressed
-            if(gamepad1.a)
-            {
-                /*
-                 * IMPORTANT NOTE: calling stopStreaming() will indeed stop the stream of images
-                 * from the camera (and, by extension, stop calling your vision pipeline). HOWEVER,
-                 * if the reason you wish to stop the stream early is to switch use of the camera
-                 * over to, say, Vuforia or TFOD, you will also need to call closeCameraDevice()
-                 * (commented out below), because according to the Android Camera API documentation:
-                 *         "Your application should only have one Camera object active at a time for
-                 *          a particular hardware camera."
-                 *
-                 * NB: calling closeCameraDevice() will internally call stopStreaming() if applicable,
-                 * but it doesn't hurt to call it anyway, if for no other reason than clarity.
-                 *
-                 * NB2: if you are stopping the camera stream to simply save some processing power
-                 * (or battery power) for a short while when you do not need your vision pipeline,
-                 * it is recommended to NOT call closeCameraDevice() as you will then need to re-open
-                 * it the next time you wish to activate your vision pipeline, which can take a bit of
-                 * time. Of course, this comment is irrelevant in light of the use case described in
-                 * the above "important note".
-                 */
-                webcam.stopStreaming();
-                //webcam.closeCameraDevice();
+            switch (pipeline.getLatestResults()) {
+                case GOAL_A:
+                    break;
+                case GOAL_B:
+                    break;
+                case GOAL_C:
+                    break;
+
             }
+
+            //reminder to use the KNO3 auto transitioner once this code is working
+            webcam.stopStreaming();
+            //webcam.closeCameraDevice();
         }
+
+
     }
-
-    /*
-     * An example image processing pipeline to be run upon receipt of each frame from the camera.
-     * Note that the processFrame() method is called serially from the frame worker thread -
-     * that is, a new camera frame will not come in while you're still processing a previous one.
-     * In other words, the processFrame() method will never be called multiple times simultaneously.
-     *
-     * However, the rendering of your processed image to the viewport is done in parallel to the
-     * frame worker thread. That is, the amount of time it takes to render the image to the
-     * viewport does NOT impact the amount of frames per second that your pipeline can process.
-     *
-     * IMPORTANT NOTE: this pipeline is NOT invoked on your OpMode thread. It is invoked on the
-     * frame worker thread. This should not be a problem in the vast majority of cases. However,
-     * if you're doing something weird where you do need it synchronized with your OpMode thread,
-     * then you will need to account for that accordingly.
-     */
-    class SamplePipeline extends OpenCvPipeline {
-        boolean viewportPaused;
-
-        /*
-         * NOTE: if you wish to use additional Mat objects in your processing pipeline, it is
-         * highly recommended to declare them here as instance variables and re-use them for
-         * each invocation of processFrame(), rather than declaring them as new local variables
-         * each time through processFrame(). This removes the danger of causing a memory leak
-         * by forgetting to call mat.release(), and it also reduces memory pressure by not
-         * constantly allocating and freeing large chunks of memory.
-         */
-
-        @Override
-        public Mat processFrame(Mat input)
-        {
-            /*
-             * Draw a simple box around the middle 1/2 of the entire frame
-             */
-            Imgproc.rectangle(
-                    input,
-                    new Point(
-                            input.cols()/4,
-                            input.rows()/4),
-                    new Point(
-                            input.cols()*(3f/4f),
-                            input.rows()*(3f/4f)),
-                    new Scalar(0, 255, 0), 4);
-
-            /**
-             * NOTE: to see how to get data from your pipeline to your OpMode as well as how
-             * to change which stage of the pipeline is rendered to the viewport when it is
-             * tapped, please see {@link PipelineStageSwitchingExample}
-             */
-
-            return input;
-
-        }
-
-        switch(pipeline.getLatestResults()) {
-            case GOAL_A:
-                break;
-            case GOAL_B:
-                break;
-            case GOAL_C:
-                break;
-
-        }
-
-        //reminder to use the KNO3 auto transitioner once this code is working
-        webcam.stopStreaming();
-        //webcam.closeCameraDevice();
-    }
-
-
 }
